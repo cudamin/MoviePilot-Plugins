95b9f24fdbd8a675c96c1a2edc4322bd89ff3dc4
cudamin <894001232@qq.com>
2026-07-30 01:44:02 +0800

    Update __init__.py

--- plugins.v2/subscribenoautosearch/__init__.py
+++ plugins.v2/subscribenoautosearch/__init__.py
@@ -17,7 +17,7 @@ class SubscribeNoAutoSearch(_PluginBase):
     plugin_version = "1.2.2"
     plugin_label = "订阅管理"
     plugin_author = "tafei"
-    author_url = "https://github.com/cudamin/MoviePilot-Plugins"
+    author_url = "https://github.com/cudamin"
     plugin_config_prefix = "subscribenoautosearch_"
     plugin_order = 19
     auth_level = 1
